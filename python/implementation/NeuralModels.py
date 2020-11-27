1d2e81c552e45fb8f406f6977fc18abe636a1b3a
Victor van Wymeersch <Victor.vanwymeersch@gmail.com>
2020-11-27 14:40:04 +0100

    Cleaned up buffers

--- python/implementation/NeuralModels.py
+++ python/implementation/NeuralModels.py
@@ -43,70 +43,71 @@ class GradAscentTrainerDiscrete(keras.models.Model):
         super(GradAscentTrainerDiscrete, self).__init__()
         self.actor_critic_net = actor_critic_net
         # self.cfg = cfg
-        self.buffer = list()  # [[s0, a, r, s1, critic], [] , ...]
-        self.action_hist = list()
-        self.critic_hist = list()
-        self.reward_hist = list()
+        self.buffer = Buffer()  # Buffer class defined below
         self.training = True
         self.training_param = training_param
 
     def addExperience(self, s0, a0, r, s1, critic):
         if self.training:
-            self.buffer.append(np.array([s0, a0, r, s1, critic]))
-            self.action_hist.append(a0)
-            self.critic_hist.append(critic)
-            self.reward_hist.append(r)
+            self.buffer.addToBuffer(s0, a0, r, s1, c)
 
     def trainStep(self):
-        print("trainStep")
-        gamma = self.training_param["gamma"]
-        optimiser = self.training_param["optimiser"]
         eps = np.finfo(np.float32).eps.item()  # Smallest number such that 1.0 + eps != 1.0
         if self.training:
             returns = []
             discounted_sum = 0
+
             # Return = SUM_t=0^inf (gamma*reward_t)
-            for r in self.reward_hist[::-1]:
-                discounted_sum = r + gamma*discounted_sum
+            for r in self.buffer.reward_hist[::-1]:
+                discounted_sum = r + self.training_param["gamma"] * discounted_sum
                 returns.insert(0, discounted_sum)
-            # Normalise
+
+            # Normalise returns
             returns = np.array(returns)
-            returns = (returns - np.mean(returns))/(np.std(returns) + eps)
+            returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
             returns = returns.tolist()
 
             # Fetch experience from buffer and calculate loss values:
-            action_vel_hist = np.array(self.action_hist)[:, 0].tolist()
-            action_off_hist = np.array(self.action_hist)[:, 1].tolist()
-            critic_hist = np.array(self.critic_hist).tolist()
-            history = zip(action_vel_hist, action_off_hist,
-                          critic_hist, returns)
+            history = zip(self.buffer.action_vel_hist, self.buffer.action_off_hist,
+                          self.buffer.critic_hist, returns)
             actor_losses_vel = []
             actor_losses_off = []
             critic_losses = []
             for actor_log_prob_vel, actor_log_prob_off, critic_val, ret in history:
                 diff = ret - critic_val
                 # if diff neg weaken, if diff pos strengthen connections
-                actor_losses_vel.append(-actor_log_prob_vel*diff)  # actor velocity loss
-                actor_losses_off.append(-actor_log_prob_off*diff)  # actor off loss
-                critic_losses.append(self.training_param["loss_function"](tf.expand_dims(critic_val, 0), tf.expand_dims(ret, 0)))
-
+                actor_losses_vel.append(-actor_log_prob_vel * diff)  # actor velocity loss
+                actor_losses_off.append(-actor_log_prob_off * diff)  # actor off loss
+                critic_losses.append(
+                    self.training_param["loss_function"](tf.expand_dims(critic_val, 0), tf.expand_dims(ret, 0)))
 
             # Backpropogation
-            with tf.GradientTape() as tape_actor:
+            loss_value_actor = sum(actor_losses_vel) + sum(actor_losses_off)
+            gradients_actor = self.tape_actor.gradient(loss_value_actor, self.actor_net.trainable_variables)
+            self.training_param["optimiser"].apply_gradients(zip(gradients_actor, self.actor_net.trainable_variables))
 
-                loss_value_actor = sum(actor_losses_vel) + sum(actor_losses_off)
-                loss_value_actor = tf.Variable(loss_value_actor, dtype=tf.float32)
-                gradients_actor = tape_actor.gradient(loss_value_actor, self.actor_net.trainable_variables)
-                optimiser.apply_gradients(zip(gradients_actor, self.actor_net.trainable_variables))
-            with tf.GradientTape() as tape_critic:
-                loss_value_critic = sum(critic_losses)
-                loss_value_critic = tf.convert_to_tensor(loss_value_critic)
-                gradients_critic = tape_critic.gradient(loss_value_critic, self.critic_net.trainable_variables)
-                optimiser.apply_gradients(zip(gradients_critic, self.critic_net.trainable_variables))
+            loss_value_critic = sum(critic_losses)
+            gradients_critic = self.tape_critic.gradient(loss_value_critic, self.critic_net.trainable_variables)
+            self.training_param["optimiser"].apply_gradients(zip(gradients_critic, self.critic_net.trainable_variables))
 
             # Clear loss values and reward history
             self.action_hist.clear()
             self.critic_hist.clear()
             self.reward_hist.clear()
 
+
+class Buffer(object):
+    def __init__(self):
+        self.state_hist = []
+        self.action_vel_hist = []
+        self.action_off_hist = []
+        self.critic_hist = []
+        self.reward_hist = []
+
+    def addToBuffer(self, s0, a0, r, s1, c):
+        self.state_hist.append(s0)
+        self.action_vel_hist.append(a0[0])
+        self.action_off_hist.append(a0[1])
+        self.critic_hist.append(c)
+        self.reward_hist.append(r)
 
