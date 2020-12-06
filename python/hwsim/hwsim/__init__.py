f6bc26747b3791b671330b5712ec96e494506574
Victor van Wymeersch <Victor.vanwymeersch@gmail.com>
2020-12-06 19:25:04 +0100

    fixed untracked files

--- python/hwsim/hwsim/__init__.py
+++
@@ -1,6 +0,0 @@
-from hwsim._wrapper import config
-from hwsim.simulation import Simulation
-from hwsim.scenario import Scenario, Road
-from hwsim.vehicle import Vehicle
-from hwsim.policy import StepPolicy, BasicPolicy, IMPolicy, ActionType, CustomPolicy
-from hwsim.model import KBModel, DBModel, CustomModel
