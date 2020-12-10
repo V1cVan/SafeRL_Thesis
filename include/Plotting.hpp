403bd0bb85c819c6cb4eb579a4615bd179700011
dcbr <[omitted]>
2020-12-10 18:17:21 +0100

    Quick fix: compilation error on older GCC compilers resolved

--- include/Plotting.hpp
+++ include/Plotting.hpp
@@ -12,11 +12,11 @@ namespace Plotting{
          * and translating them by C.
          */
         static inline void transformPoints(const Eigen::Ref<const Eigen::Matrix3Xd>& points, Eigen::Ref<Eigen::Matrix3Xd> out, const Eigen::Vector3d& C = {0,0,0}, const Eigen::Vector3d& S = {1,1,1}, const Eigen::Vector3d& A = {0,0,0}){
-            Eigen::Transform t(
+            Eigen::Affine3d t(
                 Eigen::Translation3d(C) *                           // Last: Translation
-                Eigen::AngleAxis(A[0], Eigen::Vector3d::UnitZ()) *  // Fourth: yaw rotation
-                Eigen::AngleAxis(A[1], Eigen::Vector3d::UnitY()) *  // Third: pitch rotation
-                Eigen::AngleAxis(A[2], Eigen::Vector3d::UnitX()) *  // Second: roll rotation
+                Eigen::AngleAxisd(A[0], Eigen::Vector3d::UnitZ()) *  // Fourth: yaw rotation
+                Eigen::AngleAxisd(A[1], Eigen::Vector3d::UnitY()) *  // Third: pitch rotation
+                Eigen::AngleAxisd(A[2], Eigen::Vector3d::UnitX()) *  // Second: roll rotation
                 Eigen::Scaling(S)                                   // First: Scaling
             );
             out = t * points;
