9a70e9297daa1fd29273f2d3a1566b80be05a8d7
Victor van Wymeersch <Victor.vanwymeersch@gmail.com>
2021-03-02 17:28:02 +0100

    clean up repo

--- python/hwsim/hwsim/__init__.py
+++
@@ -1,8 +0,0 @@
-from hwsim._wrapper import config
-import hwsim.serialization
-from hwsim.simulation import Simulation
-from hwsim.scenario import Scenario, Road
-from hwsim.vehicle import Vehicle
-from hwsim.policy import StepPolicy, BasicPolicy, IMPolicy, IDMConfig, MOBILConfig, SwayPolicy, TrackPolicy, ActionType, CustomPolicy
-from hwsim.model import KBModel, DBModel, CustomModel
-from hwsim.metric import Metric, metric
