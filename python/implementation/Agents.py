Final DDQN paramater sweep. Fixed Q_target standardisation formula

python/implementation/Agents.py
@@ -201,7 +201,7 @@ class DqnAgent(keras.models.Model):
 
         if self.training_param["standardise_returns"]:
             eps = np.finfo(np.float32).eps.item()
-            Q_target = Q_target - tf.math.reduce_mean(Q_target) / (tf.math.reduce_std(Q_target) + eps)
+            Q_target = (Q_target - tf.math.reduce_mean(Q_target)) / (tf.math.reduce_std(Q_target) + eps)
 
         with tf.GradientTape() as tape:
             Q_output = self.Q_actual_net(states)

python/implementation/DqnBuffers.py
@@ -206,12 +206,11 @@ class PerTrainingBuffer(object):  # stored as ( s, a, r, s_ ) in SumTree
 
     def __init__(self, buffer_size, batch_size, alpha, beta, beta_increment, use_deepset_or_cnn = False, stack_frames=False):
         self.tree = SumTree(buffer_size)
-        self.capacity = buffer_size
         self.batch_size = batch_size
         self.alpha = alpha
         self.beta = beta
         self.beta_increment = beta_increment
-        self.e = 0.01
+        self.e = np.finfo(np.float32).eps.item()
         self.use_deepset_or_cnn = use_deepset_or_cnn
         self.stack_frames = stack_frames
 
@@ -247,33 +246,28 @@ class PerTrainingBuffer(object):  # stored as ( s, a, r, s_ ) in SumTree
             idxs.append(idx)
 
         # TODO fix the division by self.tree.total() and rather just divide by the value of the root node
-        sampling_probabilities = priorities / self.tree.total()
-        is_weight = np.power(sampling_probabilities, -self.beta)
+        sampling_probabilities = np.array(priorities) / self.tree.total()
+        is_weight = (np.float32(self.tree.n_entries) * sampling_probabilities) ** (-self.beta)
         is_weight /= is_weight.max()
         is_weight = tf.squeeze(tf.convert_to_tensor(is_weight, dtype=np.float32))
 
         # Get minibatch for training
-        # TODO Remove additional for loops to speed up training like above!
         if self.use_deepset_or_cnn:
             dynamic_states = tf.squeeze(tf.convert_to_tensor([each[0] for each in mini_batch], dtype=np.float32))
             static_states = tf.squeeze(tf.convert_to_tensor([each[1] for each in mini_batch], dtype=np.float32))
             states = (dynamic_states, static_states)
-
             actions = tf.squeeze(tf.convert_to_tensor(np.array([each[2] for each in mini_batch])))
             rewards = tf.squeeze(tf.convert_to_tensor(np.array([each[3] for each in mini_batch], dtype=np.float32)))
-
             dynamic_next_states = tf.squeeze(tf.convert_to_tensor([each[4] for each in mini_batch], dtype=np.float32))
             static_next_states = tf.squeeze(tf.convert_to_tensor([each[5] for each in mini_batch], dtype=np.float32))
             next_states = (dynamic_next_states, static_next_states)
-
             done = tf.cast([each[6] for each in mini_batch], dtype=tf.float32)
         else:
             states = tf.squeeze(tf.convert_to_tensor([each[0] for each in mini_batch], dtype=np.float32))
             actions = tf.squeeze(tf.convert_to_tensor(np.array([each[1] for each in mini_batch])))
             rewards = tf.squeeze(tf.convert_to_tensor(np.array([each[2] for each in mini_batch], dtype=np.float32)))
-            next_states = tf.squeeze(tf.convert_to_tensor(np.array([each[3] for each in mini_batch], dtype=np.float32)))
+            next_states = tf.squeeze(tf.convert_to_tensor([each[3] for each in mini_batch], dtype=np.float32))
             done = tf.cast([each[4] for each in mini_batch], dtype=tf.float32)
-
         return states, actions, rewards, next_states, done, idxs, is_weight
 
     def update(self, idx, error):
@@ -282,7 +276,7 @@ class PerTrainingBuffer(object):  # stored as ( s, a, r, s_ ) in SumTree
             self.tree.update(idx, p)
 
     def is_buffer_min_size(self):
-        return self.get_size() > self.batch_size+64
+        return self.get_size() > self.batch_size
 
     def get_size(self):
         return self.tree.n_entries

python/implementation/GraphPlotter.py
@@ -22,6 +22,9 @@ For deleting experiment from Tensorboard:
 
 # tensorboard dev delete --experiment_id "nQWFNCwYSPWCJzy04EabMA"
 # tensorboard dev upload --logdir "./logfiles/DDQN_ER_tuning/train" --name "DDQN ER Tuning run 2" --description "DDQN ER Tuning parameter sweep 2. Default var: Policy action rate=8; Units=(32,32); Activation=relu; Batch size=32; Learning rate=0.0001; Loss func=Huber; Clip grads=False; Gamma=0.95; Target update rate=1e4; Standardise returns=False"
+# tensorboard dev upload --logdir "./logfiles/DDQN_Epsilon_Beta_tuning/train" --name "Epsilon tuning sweep" --description "DDQN Epsilon parameter sweep. Default var: Policy action rate=8; Units=(32,32); Activation=relu; Batch size=32; Learning rate=0.0001; Loss func=Huber; Clip grads=False; Gamma=0.95; Target update rate=1e4; Standardise returns=False; Buffer size=300000"
+# tensorboard dev upload --logdir "./logfiles/DQN_DDQN_standardisation_target_update/train" --name "DQN with target standardisation vs DDQN" --description "Comparison with shorter learning times and less exploration. Default var: Policy action rate=8; Units=(32,32); Activation=relu; Batch size=32; Learning rate=0.0001; Loss func=Huber; Clip grads=False; Gamma=0.95; Target update rate=1e4; Standardise returns=False; Buffer size=300000"
+# tensorboard dev upload --logdir "./logfiles/DQN_DDQN_standardisation_target_update_long_run/train" --name "DQN with target standardisation vs DDQN - long run" --description "Comparison with longer learning times and more exploration."
 
 def start_run(parameter, tag_value, df, fig_path):
 
@@ -32,11 +35,14 @@ def start_run(parameter, tag_value, df, fig_path):
     plot_df = df.loc[(df['parameter_tested'] == parameter) & (df['tag'] == tag_value)]
     # plot_df = plot_df.iloc[::10,:] #Clearer by not plotting every step
     ax = sns.lineplot(x="step", y="smoothed",
-                 hue="description",
-                 data=plot_df,
-                 alpha=0.6,
-                 linewidth=1.5)
-    ax.legend(loc='lower right', title='Parameter:')
+                  hue="description",
+                  data=plot_df,
+                  alpha=0.6,
+                  linewidth=1.8)
+    if tag_value == "Epsilon":
+        ax.get_legend().remove()
+    else:
+        ax.legend(loc='lower right', title='Parameter:')
     ax.set(xlabel = "Episode")
     ax.set(ylabel = tag_value)
     plt_name = fig_path + "/" + parameter + '-' + tag_value + '.png'
@@ -83,7 +89,7 @@ def preprocess_data(csv_path):
     df = extract_columns(df)
     # df = df.loc[df['tag'].isin(['Mean episode reward', 'Mean vehicle speed for episode'])]
     # df['parameter_tested'] = df['description'].str.extract(r'(.*?)-')
-    df = df.loc[df['tag'].isin(['Reward', 'Vehicle speed'])]
+    df = df.loc[df['tag'].isin(['Reward', 'Vehicle speed', 'Epsilon'])]
     df['parameter_tested'] = df['description'].str.extract(r'(.*?)=')
 
 
@@ -100,7 +106,10 @@ def preprocess_data(csv_path):
                 smooth_np = np.empty_like(raw_np)  # Initialize smooth_data_array
                 # Initialise values
                 smooth_np[0] = raw_np[0]  # First value in smoothed array is same as first value in raw
-                weight = 0.95
+                if k == "Epsilon":
+                    weight = 0
+                else:
+                    weight = 0.95
                 for step in range(1, len(raw_np)):
                     smooth_np[step] = (1 - weight) * raw_np[step] + weight * smooth_np[step - 1]
                 # Add np_array to df
@@ -116,19 +125,25 @@ def preprocess_data(csv_path):
 if __name__ == "__main__":
     experiment_ids = {
         "DDQN_ER_initialisers": "8viGslanQLWtMaORGizRaQ",
-        "DDQN_ER_tuning": "ndalAuYXRxGuML7vkgPA4Q"
+        "DDQN_ER_tuning": "ndalAuYXRxGuML7vkgPA4Q",
+        "Epsilon_sweep": "t1GY8hx8QFWiNjyofqJUNw",
+        "DQN_DDQN_standardisation_short": "0o0v4ZgrTWWUnOUlBrWmow",
+        "DQN_DDQN_standardisation_long": "rhGbDukaTL28a8hjZO6QiQ",
     }
 
     experiment_paths = {
         "DDQN_ER_initialisers": "./logfiles/DDQN_ER_initialisers",
-        "DDQN_ER_tuning": "./logfiles/DDQN_ER_tuning/train"
+        "DDQN_ER_tuning": "./logfiles/DDQN_ER_tuning/train",
+        "Epsilon_sweep": "./logfiles/DDQN_Epsilon_Beta_tuning/train",
+        "DQN_DDQN_standardisation_short": "./logfiles/DQN_DDQN_standardisation_target_update/train",
+        "DQN_DDQN_standardisation_long": "./logfiles/DQN_DDQN_standardisation_target_update_long_run/train",
     }
 
     experiment_names = list(experiment_ids.keys())
     print(experiment_names)
 
-    experiment_name = "DDQN_ER_tuning"
-    csv_path = experiment_paths[experiment_name] + "/" + experiment_names[1] + '.csv'
+    experiment_name = "DQN_DDQN_standardisation_long"
+    csv_path = experiment_paths[experiment_name] + "/" + experiment_name + '.csv'
     download_and_save(experiment_name, csv_path)  # Comment out if you don't want to re-download the data
 
     # Pre-process (smooth data from csv)
@@ -142,10 +157,9 @@ if __name__ == "__main__":
 
     def param_gen():
         # This function should return (yield because it's a generator) all parameter combinations you want to try
-        # for parameter in df['parameter_tested'].unique():
-        parameter = "Buffer size"
-        for tag_value in df['tag'].unique():
-            yield parameter, tag_value, df, experiment_paths[experiment_name]
+        for parameter in df['parameter_tested'].unique():
+            for tag_value in df['tag'].unique():
+                yield parameter, tag_value, df, experiment_paths[experiment_name]
 
     if procs > 1:
         # Schedule all training runs in a parallel loop when using multiple cores:
@@ -159,7 +173,5 @@ if __name__ == "__main__":
         # To make debugging easier when using 1 core, a classical for loop:
         for args in param_gen():
             start_run(*args)
-
-
 
     print("EOF")

python/implementation/Main.py
@@ -134,7 +134,7 @@ class Main(object):
                     if t % self.training_param["policy_rate"] == 0:
                         train_counter += 1
                         self.policy.agent.epsilon_decay_count = train_counter
-                        self.policy.agent.timestep = np.int(t / self.training_param["policy_rate"])
+                        self.policy.agent.timestep = int(t / self.training_param["policy_rate"])
                         if self.policy.agent.buffer.is_buffer_min_size():
                             model_update_counter += 1
                             if model_update_counter % self.training_param["model_update_rate"] == 0:
@@ -157,13 +157,12 @@ class Main(object):
 
             time_taken_episode = self.episodeTimer.endTime()
 
-            if trained:
-                if 0.05 * reward + (1 - 0.05) * running_reward > running_reward and episode_count % 50 == 0:
-                    self.policy.agent.Q_actual_net.save_weights(self.model_param["weights_file_path"])
-                    print("Saved network weights.")
+            # if trained and episode_count % 50 == 0:
+            #     self.policy.agent.Q_actual_net.save_weights(self.model_param["weights_file_path"])
+            #     print("Saved network weights.")
 
-                # Running reward smoothing effect
-                running_reward = 0.05 * reward + (1 - 0.05) * running_reward
+            # Running reward smoothing effect
+            running_reward = 0.05 * reward + (1 - 0.05) * running_reward
 
 
             if episode_count % 50 == 0 and trained:
@@ -198,7 +197,10 @@ class Main(object):
             self.tb_logger.save_variable("Vehicle speed", x=episode_count, y=np.mean(vehicle_speeds))
             if (episode_count % 25 == 0 or episode_count == 1):
                 if self.training_param["use_per"]:
-                    self.tb_logger.save_variable("Beta increment", x=episode_count, y=beta)
+                    try:
+                        self.tb_logger.save_variable("Beta increment", x=episode_count, y=beta)
+                    except:
+                        self.tb_logger.save_variable("Beta increment", x=episode_count, y=self.policy.agent.buffer.beta)
                 else:
                     try:
                         self.tb_logger.save_variable(name='Epsilon', x=episode_count, y=epsilon)
@@ -441,7 +443,7 @@ def start_run(arg0, arg1, arg2, arg3):
     # ACT_FUNC = tf.nn.relu
     N_STACKED_TIMESTEPS = 2  # TODO Check stacked timesteps (shouldnt it be 4? )
     BATCH_NORM = False
-    MODEL_FILE_PATH = "logfiles/"+RUN_NAME+"/"+"train"+"/Seed"+str(SEED)+"-Details="+str(RUN_INFO)+"/model_weights"
+    MODEL_FILE_PATH = "logfiles/"+RUN_NAME+"/"+"train"+"/Seed"+str(SEED)+"-Details="+str(RUN_INFO) + "/"
     if RUN_TYPE == "train":
         TRAINABLE = True
     elif RUN_TYPE == "test":
@@ -487,7 +489,7 @@ def start_run(arg0, arg1, arg2, arg3):
     """TRAINING PARAMETERS:"""
     POLICY_ACTION_RATE = 8  # Number of simulator steps before new control action is taken
     MAX_TIMESTEPS = 5e3  # range: 5e3 - 10e3
-    MAX_EPISODES = 1500
+    MAX_EPISODES = 1500 # 800
     FINAL_RETURN = 1
     SHOW_TRAIN_PLOTS = False
     SAVE_TRAINING = True
@@ -502,7 +504,7 @@ def start_run(arg0, arg1, arg2, arg3):
     # BATCH_SIZE = 32  # range: 32 - 150
     EPSILON_MIN = 1  # Exploration
     EPSILON_MAX = 0.1  # Exploitation
-    DECAY_RATE = 0.99999  # 0.999986
+    DECAY_RATE = 0.99999  # 0.99997
     MODEL_UPDATE_RATE = 1
     # TARGET_UPDATE_RATE = 1e4
     # LEARN_RATE = 0.0001  # range: 1e-3 - 1e-4
@@ -516,11 +518,11 @@ def start_run(arg0, arg1, arg2, arg3):
     # STANDARDISE_RETURNS = False
     USE_PER = False
     ALPHA = 0.75  # Priority scale: a=0:random, a=1:completely based on priority
-    BETA = 0.2  # Prioritisation factor
+    BETA = 0.3  # Prioritisation factor
     BETA_INCREMENT = 0.00004  # Rate of Beta annealing to 1
     # Model types:
-    USE_TARGET_NETWORK = True
-    USE_DUELLING = False
+    # USE_TARGET_NETWORK = True
+    USE_DUELLING = True
     USE_DEEPSET = False
     USE_CNN = False  # TODO Fix so that CNN type 3 and LSTM work with PER!
     USE_LSTM = False
@@ -679,7 +681,6 @@ if __name__=="__main__":
                      "Activation function", "Model size", "Clip gradients", "Discount factor"):
             for arg2 in (100, 300, 500):
                 if arg1 == "Batch size":
-                    # for arg3 in (32, 64, 128):
                     for arg3 in (32, 64, 128):
                         yield arg0, arg1, arg2, arg3
                 elif arg1 == "Learning rate":
@@ -705,6 +706,12 @@ if __name__=="__main__":
                         yield arg0, arg1, arg2, arg3
                 elif arg1 == "Standardise returns":
                     for arg3 in (True, False):
+                        yield arg0, arg1, arg2, arg3
+                elif arg1 == "Beta increment":
+                    for arg3 in (3.2e-6, 2.13e-6, 1.6e-6, 1.1e-6):
+                        yield arg0, arg1, arg2, arg3
+                elif arg1 == "Epsilon decay rate":
+                    for arg3 in (0.99997, 0.99998, 0.999985, 0.99999):
                         yield arg0, arg1, arg2, arg3
                 else:
                     sys.exit()